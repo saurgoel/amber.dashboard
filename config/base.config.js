cbc680343e9a731ae97ca0127dcc32449f097697
Tushar Kant <nanu.clickity@gmail.com>
2015-12-04 01:30:14 +0530

    base web pack config

---
+++ config/base.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+module.exports = function(env){
+	return {
+		cache: !env.is_prod,
+		debug: !env.is_prod,
+
+		output: {
+			publicPath: '/',
+			sourcePrefix: '  ',
+		},
+
+		resolve: {
+			extensions: ['', '.webpack.js', '.web.js', '.js', '.json'],
+		},
+
+		module: {
+			loaders: [
+				{ test: /\.json$/, loader: 'json-loader' }, 
+				{ test: /\.txt$/, loader: 'raw-loader' }, 
+				{ test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/, loader: 'url-loader?limit=10000' },
+				{ test: /\.(eot|ttf|wav|mp3)$/, loader: 'file-loader' }
+			]
+		},
+
+		plugins: [
+			new webpack.optimize.OccurenceOrderPlugin()
+		]
+	}
+};
