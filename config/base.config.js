48b202e62ec8244657d3b00792f55999e21e09c5
Tushar Kant <nanu.clickity@gmail.com>
2015-12-04 23:54:34 +0530

    font awesome loaders

--- config/base.config.js
+++ config/base.config.js
@@ -17,7 +17,8 @@ module.exports = function(env){
 		module: {
 			loaders: [
 				{ test: /\.json$/, loader: 'json-loader' }, 
-				{ test: /\.txt$/, loader: 'raw-loader' }, 
+				{ test: /\.txt$/, loader: 'raw-loader' },
+				{ test: /\.(woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000' },
 				{ test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/, loader: 'url-loader?limit=10000' },
 				{ test: /\.(eot|ttf|wav|mp3)$/, loader: 'file-loader' }
 			]
