00196cfe6e3eb2c00d58fd37bcca070bd719677e
Tushar Kant <nanu.clickity@gmail.com>
2015-12-04 23:52:51 +0530

    header view update

--- src/client/views/header/index.js
+++ src/client/views/header/index.js
@@ -1,11 +1,29 @@
 import {ItemView} from 'marionette';
 import tpl from './template.jade';
 
+import Radio from 'radio';
+
 import style from './style.styl';
 
-class HeaderView extends ItemView {
-	template  = tpl;
-	className = 'view-header' 
-}
+var HeaderView = ItemView.extend({
+	template: tpl,
+	className: 'view-header',
+
+	ui: {
+		menuIcon: '#sidebar-toggle'
+	},
+	events: {
+		'click @ui.menuIcon': 'toggleSidebar'
+	},
+
+	initialize(){
+		this.listenTo(this, 'render' , style.use);
+		this.listenTo(this, 'destroy', style.unuse);
+	},
+
+	toggleSidebar(e){
+		Radio.trigger('global', 'sidebar:toggle')
+	}
+});
 
 export default HeaderView;
