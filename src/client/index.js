07590fc11caa8f283ba93de722b40f623b9a4517
Tushar Kant <nanu.clickity@gmail.com>
2015-12-08 04:55:43 +0530

    app update

--- src/client/index.js
+++ src/client/index.js
@@ -7,26 +7,22 @@ import App from './app';
 window.$ = window.jQuery = $;
 window._ = _;
 
-// Debug Stuff
-(function(){
-	if (__DEV__){
-		Radio.DEBUG = true;
-		if (window.__agent) window.__agent.start(Backbone, Marionette);
-	}
-})();
 
 
-
+// App
 let app = window.Amber = new App();
-// Start after fetching config
 Promise.resolve($.getJSON('/config'))
 	.then( config => {
-		Radio.reply('global', 'config');
+		Radio.reply('global', 'config', config);
 		app.start(config);
 	})
-	.catch( console.error.bind(console, 'ERROR:\n') )
+	.catch( err => {
+		console.error('APPERROR: ', err);
+	});
 
 
+
+// HMR Fix
 if (module.hot){
 	module.hot.accept();
 }
