3a8aa2b308af1bc0d08f90b5bbe8e94ce31a4a81
Tushar Kant <nanu.clickity@gmail.com>
2015-12-07 19:58:22 +0530

    move up notification panel

--- src/client/views/sidebar/model.js
+++ src/client/views/sidebar/model.js
@@ -5,10 +5,10 @@ const menus = [
 	{name: 'Dashboard',  route: '#/dashboard',  icon: 'dashboard' },
 	{name: 'Customer',   route: '#/customer',   icon: 'users' },
 	{name: 'Product' ,   route: '#/product' ,   icon: 'product-hunt'},
+	{name: 'Notification', route: '#/notification', icon: 'bell'},
 	{name: 'Location',   route: '#/location',   icon: 'map-marker'},
 	{name: 'Images'  ,   route: '#/images'  ,   icon: 'picture-o'},
 	{name: 'Client'  ,   route: '#/client'  ,   icon: 'black-tie'},
-	{name: 'Notification', route: '#/notification', icon: 'bell'}
 ]
 
 class SidebarModel extends Model {
