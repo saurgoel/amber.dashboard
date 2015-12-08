8bf2febe112890529ee6882fec712ef3f7d7fddd
Tushar Kant <nanu.clickity@gmail.com>
2015-12-08 15:15:00 +0530

    ui update

--- src/client/views/dashboard/index.js
+++ src/client/views/dashboard/index.js
@@ -3,9 +3,11 @@ import {ItemView} from 'marionette';
 import tpl from './template.jade';
 import style from './style.styl';
 
+import {useCSS} from '../../utils';
 
+@useCSS(style)
 class DashboardPage extends ItemView {
-	template  = tpl;
+	template  = tpl
 	className = 'view-dashboard-page'
 }
 
