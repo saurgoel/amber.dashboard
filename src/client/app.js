90bdeb9443e9b0c77062f44dd3396f371ea579ea
Tushar Kant <nanu.clickity@gmail.com>
2015-12-11 16:34:46 +0530

    catch a[href] links in internal navigation

--- src/client/app.js
+++ src/client/app.js
@@ -27,6 +27,7 @@ class App extends Application{
 	onBeforeStart(){
 		this.setupBehaviors();
 		this.ajaxConfig();
+		this.handleHref();
 		this.initRouter();
 	}
 
@@ -34,6 +35,7 @@ class App extends Application{
 		let counter = 0;
 		$.ajaxSetup({
 			crossdomain: true,
+			xhrFields: { withCredentials: true },
 			beforeStart: ()=> {
 				Radio.trigger('global', 'loader:start');
 				counter++;
@@ -52,6 +54,30 @@ class App extends Application{
 
 	initRouter(){
 		this.Router = new AppRouter({controller: new AppController()})
+	}
+
+	handleHref(){
+		$(document).on('click', 'a[href^="/"]', (e)=>{
+			let $target = $(e.currentTarget)
+			let href = $target.attr('href')
+			
+			// <a href='/some/route' data-pass=true>
+			if ($target.data('pass')) return;
+
+			// chain 'or's for other black list routes
+			let passThrough = href.indexOf('sign_out') >= 0
+
+			// Allow shift+click for new tabs, etc.
+			if( !passThrough && !e.altKey && !e.ctrlKey && !e.metaKey && !e.shiftKey){
+				e.preventDefault()
+			}
+
+			// Remove leading slashes and hash bangs (backward compatablility)
+			let url = href.replace(/^\//  , '').replace('\#\!\/', '');
+			// Instruct Backbone to trigger routing events
+			this.Router.navigate(url, {trigger: true})
+			return false
+		});
 	}
 
 	onStart(){
