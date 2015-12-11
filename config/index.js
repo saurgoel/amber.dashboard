01d442d6c96b582e6287f77e203cc611c91f68bd
Tushar Kant <nanu.clickity@gmail.com>
2015-12-11 16:36:45 +0530

    added notifications api url to config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 export default {
 	port: 4200,
 	api_base: 'http://product.getamber.com/',
-	accounts_api_url: 'http://clients.getamber.com'
+	accounts_api_url: 'http://clients.getamber.com',
+	notifications_api_url: 'http://notify.getamber.com'
 };
