043024fecb97bafde39b105a0f9f8ed040a49a43
Tushar Kant <nanu.clickity@gmail.com>
2015-12-11 16:35:17 +0530

    load sub panels through notification layout view

--- src/client/panels/notification/index.js
+++ src/client/panels/notification/index.js
@@ -10,31 +10,46 @@ import SubHeader from './subheader';
 var NotificationPanelLayout = LayoutView.extend({
 	template : tpl,
 	className: 'layout-notification-panel',
-
+	regions: {
+		Sidebar: '#region-notification-sidebar',
+		Content: '#region-notification-content'
+	},
 	sidebar_links: [
 		{ name: 'Emails',  href: '/notification/emails' },
 		{ name: 'SMS', href: '/notification/sms' },
 		{ name: 'Push Notifications', href: '/notification/push' },
 	],
 	
-	initialize(){
+	initialize({subpanel}){
+		this.subpanel = subpanel;
+
 		this.listenTo(this, 'render', style.use);
 		this.listenTo(this, 'destroy', style.unuse);
-
-		this.listenToOnce(this, 'render', this.initSubHeader);
-
-		// this.listenTo(this, 'render', this.renderViews)
-		console.log('Notification Panel Layout Ready')
+		
+		this.listenTo(this, 'attach', this.renderViews);
+		
+		console.log('Notification Panel Layout Ready');
 	},
 
 	renderViews(){
-		this.sidebarView = this.sidebarView || new SideNav({collection: this.sidebar_links});
-		this.getRegion('Sidebar').show( this.sidebarView )
+		this.initSubHeader();
+		
+		// Sidebar
+		let collection = new Collection(this.sidebar_links);
+		this.getRegion('Sidebar').show( new SideNav({collection}) );
+
+		// Subpanel
+		if (!this.subpanel) return
+		let region = this.getRegion('Content');
+		require.ensure([], ()=>{
+			var view = require('./' + this.subpanel + '/index.js');
+			this.getRegion('Content').show(new view());
+		});
 	},
 
 	initSubHeader(){
 		this.subheaderView = new SubHeader();
-		Radio.trigger('header', 'update:subheader:title', 'Notification Service')
+		Radio.trigger('header', 'update:subheader:title', 'Notification')
 		Radio.trigger('header', 'update:subheader', this.subheaderView);
 	}
 
