57cce0f4a7df6467b3b9552df4ffaa6f224d47c2
Tushar Kant <nanu.clickity@gmail.com>
2015-12-11 16:35:57 +0530

    notification panel subhead

--- src/client/panels/notification/subheader/index.js
+++ src/client/panels/notification/subheader/index.js
@@ -1,11 +1,27 @@
 import {ItemView} from 'marionette';
 import tpl from './template.jade';
 
+import style from './style.styl';
 
 var NotificationSubHeader = ItemView.extend({
 	template: tpl,
 	className: 'view-notification-subheader',
 
+	templateHelpers(){
+		return ()=> { 
+			let locals = ()=> style.locals ? style.locals : style.use().locals;
+			return {locals}
+		}
+	},
+
+	initialize(){
+		this.listenTo(this, 'render', style.use);
+		this.listenTo(this, 'destroy', style.unuse);
+	},
+	
+	onAttach(){
+		this.$('.tabs').tabs();
+	}
 
 });
 
