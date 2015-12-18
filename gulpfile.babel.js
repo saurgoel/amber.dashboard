741d08eb5dd317e9c034713fc7de3349064beb06
Tushar Kant <nanu.clickity@gmail.com>
2015-12-18 14:15:02 +0530

    hot reloads for server and client both

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,77 +1,116 @@
+import fs from 'fs';
+import path from 'path';
+
+import cp from 'child_process';
 import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import del from 'del';
+import mkdirp from 'mkdirp';
+import runSequence from 'run-sequence';
+
 import minimist from 'minimist';
-import merge from 'lodash/object/merge';
-import {exec} from 'child-process-promise';
-import nodemon from 'gulp-nodemon';
 import nodeInspector from 'gulp-node-inspector';
+import nodemon from 'gulp-nodemon';
 import browserSync from 'browser-sync';
+
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from './webpack.config';
 
-
+const $ = gulpLoadPlugins();
 const argv = minimist(process.argv.slice(2));
-const WATCH = argv.watch ? true : false;
-const PRODUCTION = process.env.NODE_ENV === 'production' || (argv.prod ? true : false);
+const isProduction = !!argv.prod;
 
+const config  = require('./webpack.config.js');
+const serverBundler = webpack(config[1]);
+const clientBundler = webpack(config[0]);
 
-console.log(`GULP ENV: { watch: ${WATCH}, production: ${PRODUCTION} }`);
+var watch = false;
+var src = {
+  assets: [
+    'src/public/**'
+  ],
+  resources: [
+    'package.json',
+    'src/server/templates*/**'
+  ]
+};
 
-var clientbundler, serverbundler;
+var dest = {
+  build: 'build',
+  logs:  'build/logs',
+  public: 'build/public'
+};
 
-gulp.task('default', ['reload']);
-
-gulp.task('clean', cb => {
-	var cmd = [
-		'rm -rf ./build',
-		'mkdir -p ./build/public'
-	].join(' && ')
-
-	exec(cmd)
-		.then( result => {
-			console.log('clean:stdout', result.stdout)
-			return cb();
-		})
-		.catch( err => console.error('clean:error', err))
-});
-
-gulp.task('copy', cb => {
-	gulp
-		.src('./src/public/**')
-		.pipe(gulp.dest('./build/public'))
-	return (
-		gulp
-		.src('./src/server/templates/**')
-		.pipe(gulp.dest('./build/templates'))
-	);
-});
-
-gulp.task('build', ['clean', 'copy', 'webpack:build']);
-
-
-gulp.task('webpack:build', ['clean', 'copy'], cb => {
-	let runCount = 0;
-	clientbundler = webpack(config[0]);
-	serverbundler = webpack(config[1]);
-	
-	let onComplete = (err, stats)=>{
-		if (err) return console.error(err);
-		console.log(stats.toString({colors: true, chunks: false}))
-		runCount += 1;
-		if (runCount === 2) return cb();
-	}
-	if (WATCH){
-		clientbundler.watch(200, onComplete)
-		serverbundler.watch(200, onComplete)
-	}
-	else {
-		clientbundler.run(onComplete)
-		serverbundler.run(onComplete)	
-	}
+gulp.task('clean', async (cb) => {
+  await del(['.tmp', 'build/*'], {dot: true});
+  await mkdirp(dest.logs);
+  await mkdirp(dest.public);
+  return cb;
 });
 
 
+gulp.task('assets', ['clean'], (cb) => {
+  return gulp.src(src.assets)
+    .pipe($.changed(dest.public))
+    .pipe(gulp.dest(dest.public))
+    .pipe($.size({title: 'assets'}));
+});
+
+// Resource files
+gulp.task('resources', () => {
+  return gulp.src(src.resources)
+    .pipe($.changed(dest.build))
+    .pipe(gulp.dest(dest.build))
+    .pipe($.size({title: 'resources'}));
+});
+
+
+// Bundle
+gulp.task('bundle', ['clean', 'assets', 'resources'], cb => {
+  let runCount = 0;
+  function bundlerCallback(err, stats){
+    if (err)
+      throw new $.util.PluginError('webpack', err);
+    console.log(stats.toString(config[0].stats));
+    if (++runCount === (watch ? config.length : 1)) {
+      return cb();
+    }
+  }
+  if (watch){
+    serverBundler.watch(200, bundlerCallback);
+    clientBundler.watch(200, bundlerCallback);
+  }
+  else{
+    serverBundler.run(bundlerCallback);
+    clientBundler.run(bundlerCallback);
+  }
+});
+
+// Build and start watching for modifications
+gulp.task('build:watch', cb => {
+  watch = true;
+  runSequence(['bundle'], ()=>{
+    gulp.watch(src.assets, ['assets']);
+    gulp.watch(src.resources, ['resources']);
+    cb();
+  });
+});
+
+// Launch a Node.js/Express server
+gulp.task('serve', ['build:watch', 'node-debug'], cb => {
+  console.log('Starting Node Server...');
+  nodemon({
+    script: 'build/server.js',
+    watch : 'build/server.js',
+    env: Object.assign({NODE_ENV: 'development'}, process.env),
+    nodeArgs: ['--debug']
+  }).on('restart', cb=>{
+    console.log('Server Restarted: Reloading BrowserSync.');
+    browserSync.reload();
+  });
+  cb();
+});
 
 gulp.task('node-debug',  cb => {
   gulp.src([]).pipe( nodeInspector({preload:false}) );
@@ -80,36 +119,29 @@ gulp.task('node-debug',  cb => {
 
 
 // Launch BrowserSync development server
-gulp.task('reload', ['node-debug', 'server', 'build'], cb => {
+gulp.task('sync', ['serve'], cb => {
 
   process.on('exit', () => browserSync.exit());
 
   browserSync({
-    logPrefix: 'Amber: ',
+    logPrefix: 'Housing: ',
     open: false, notify: true, https: false,
     proxy: {
       target: 'localhost:4200',
       middleware: [
-        webpackDevMiddleware(clientbundler, {
+        webpackDevMiddleware(clientBundler, {
           publicPath: config[0].output.publicPath,
-          stats: {colors: true, chunks: false}
+          stats: false
         }),
-        webpackHotMiddleware(clientbundler)
+        webpackHotMiddleware(clientBundler)
       ]
-    }
+    },
+    files: [
+      'build/public/**/*.css',
+      '!build/public/**/*.js'
+    ]
   }, cb);
 });
 
 
-gulp.task('server', ['build'], cb => {
-	nodemon({
-		script: './build/server.js',
-		watch:  './build/server.js',
-		env: merge({NODE_ENV: 'development'}, process.env),
-    nodeArgs: ['--debug']
-  }).on('restart', cb=>{
-    console.log('Server Restarted: Reloading BrowserSync.');
-    browserSync.reload();
-  });
-  return cb();
-});
+gulp.task('default', ['sync']);
