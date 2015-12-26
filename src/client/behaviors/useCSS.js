c882848013e664dc088e1e21b0d068a2ed9d6706
Tushar Kant <nanu.clickity@gmail.com>
2015-12-27 02:30:29 +0530

    view's el will inherit .self from local styles automatically
    `:local .self` will be applied to used view’s el directly

--- src/client/behaviors/useCSS.js
+++ src/client/behaviors/useCSS.js
@@ -1,4 +1,4 @@
-import {extend} from 'underscore';
+import _ from 'underscore';
 import {Behavior} from 'marionette';
 
 
@@ -17,10 +17,18 @@ var UseCSS = Behavior.extend({
 		var locals = this.options.style.locals;
 		_.isUndefined(th)
 			? this.view.templateHelpers = {s:locals}
-			: this.view.templateHelpers = extend({}, th, {s:locals})
+			: this.view.templateHelpers = _.extend({}, th, {s:locals});
+
+		// Export of .self
+		// directly to this.el element
+		// or extend this.className otherwise
+		let self_export = _.has(locals, 'self') ? locals.self : '';
+		this.view.isRendered
+			? this.view.el.className += ` ${self_export}`
+			: this.view.className += ` ${self_export}`;
 	},
 
-	onBeforeDestroy(){ 
+	onBeforeDestroy(){
 		if (!this.options.style) return;
 		this.options.style.unuse();
 	}
