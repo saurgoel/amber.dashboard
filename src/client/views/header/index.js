d91859e243ccf229cd3f58568cac26f07b6cd006
Tushar Kant <nanu.clickity@gmail.com>
2015-12-26 18:37:46 +0530

    header update

--- src/client/views/header/index.js
+++ src/client/views/header/index.js
@@ -6,74 +6,27 @@ import style from './style.styl';
 
 const HeaderChannel = Radio.channel('header');
 
-class HeaderView extends LayoutView {
 
-	get className(){ return 'view-header' }
-	
-	template = tpl
-	
-	regions = {
+var HeaderView = LayoutView.extend({
+	template: tpl,
+	className: 'view-header',
+	regions: {
 		SubHeader: '#region-sub-header'
-	}
-	
-	ui = {
-		menuIcon: '#sidebar-toggle',
-		subheaderTitle: '.sub-header-title'
-	}
-
-	events = {
+	},
+	ui: {
+		menuIcon: '.sidebar-toggle',
+	},
+	events: {
 		'click @ui.menuIcon': 'toggleSidebar'
-	}
+	},
 
 	initialize(){
 		this.listenTo(this, 'render' , style.use);
 		this.listenTo(this, 'destroy', style.unuse);
-		
-		this.listenTo(HeaderChannel, 'update:subheader', this.updateSubheader);
-		this.listenTo(HeaderChannel, 'update:subheader:title', this.updateSubheaderTitle);
-	}
-	updateSubheader(viewInstance){
-		this.getRegion('SubHeader').show(viewInstance);
-	}
-	updateSubheaderTitle(title){
-		this.ui.subheaderTitle.text(title ? title : 'Title');
-	}
+	},
 	toggleSidebar(e){
 		Radio.trigger('global', 'sidebar:toggle')
 	}
-
-}
-
-// var HeaderView = LayoutView.extend({
-// 	template: tpl,
-// 	className: 'view-header',
-// 	regions: {
-// 		SubHeader: '#region-sub-header'
-// 	},
-// 	ui: {
-// 		menuIcon: '#sidebar-toggle',
-// 		subheaderTitle: '.sub-header-title'
-// 	},
-// 	events: {
-// 		'click @ui.menuIcon': 'toggleSidebar'
-// 	},
-
-// 	initialize(){
-// 		this.listenTo(this, 'render' , style.use);
-// 		this.listenTo(this, 'destroy', style.unuse);
-		
-// 		this.listenTo(HeaderChannel, 'update:subheader', this.updateSubheader);
-// 		this.listenTo(HeaderChannel, 'update:subheader:title', this.updateSubheaderTitle);
-// 	},
-// 	updateSubheader(viewInstance){
-// 		this.getRegion('SubHeader').show(viewInstance);
-// 	},
-// 	updateSubheaderTitle(title){
-// 		this.ui.subheaderTitle.text(title ? title : 'Title');
-// 	},
-// 	toggleSidebar(e){
-// 		Radio.trigger('global', 'sidebar:toggle')
-// 	}
-// })
+})
 
 export default HeaderView;
