ed0dd334f0848774c24c48cf1528f2a6a595c2b1
Tushar Kant <nanu.clickity@gmail.com>
2015-12-28 18:23:16 +0530

    fix auth redirection

--- src/server/helpers/token_validator.js
+++ src/server/helpers/token_validator.js
@@ -2,11 +2,10 @@ import request from 'request';
 import config from '../config';
 
 const TokenValidator = (req, res, next)=>{
-	var host  = __DEV__ ? 'local.getamber.com:3000' : req.get('host');
 	var token = req.cookies.auth_token || null;
-	
+
 	var validateURL = `${config.accounts_api_url}/auth/accounts/validate?auth_token=${token}`;
-	var redirectURL = `${config.accounts_api_url}/?redirect_to=${req.protocol}://${host+req.originalUrl}`;
+	var redirectURL = `${config.accounts_api_url}/?redirect_to=${req.protocol}://${req.get('host')+req.originalUrl}`;
 
 	request(validateURL, function(err, response, body) {
 		var valid = false;
@@ -28,8 +27,8 @@ const TokenValidator = (req, res, next)=>{
 				else { res.json({status: 'error', message: 'Invalid Token'}) }
 			},
 			text: ()=> {
-				if(err) { next(err); } 
-				else if(valid) { next(); } 
+				if(err) { next(err); }
+				else if(valid) { next(); }
 				else { res.send("error: Invalid Token"); }
 			}
 		});
