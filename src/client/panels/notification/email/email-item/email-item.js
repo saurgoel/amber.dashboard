c4a5b8e971a7e6d79beab84b26e948fb18a32959
Tushar Kant <nanu.clickity@gmail.com>
2015-12-28 17:52:29 +0530

    update email view upon selected from list

--- src/client/panels/notification/email/email-item/email-item.js
+++ src/client/panels/notification/email/email-item/email-item.js
@@ -3,12 +3,16 @@ import {WithCSS} from 'utils';
 
 import tpl from './template-email.jade';
 
-
+var EmailChannel = Radio.channel('email');
 
 class EmailItem extends ItemView {
   template = tpl
   initialize(){
-
+    this.listenTo(EmailChannel, 'item:selected', this.updateEmail);
+  }
+  updateEmail(model){
+    this.model = model;
+    this.render();
   }
 }
 
