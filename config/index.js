f327bc49625ba0ddbd9c30995c82bd7fa9dd535e
Tushar Kant <nanu.clickity@gmail.com>
2015-12-29 00:01:50 +0530

    deploy fix

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ export default {
   serverPort: 4100,
   bsPort: 4200,
   api_base: 'https://product.getamber.com/',
-  accounts_api_url: 'http://clients.internal.getamber.com',
+  accounts_api_url: 'https://clients.getamber.com',
+  accounts_api_internal_url: 'http://clients.internal.getamber.com',
   notifications_api_url: 'https://notify.getamber.com'
 };
