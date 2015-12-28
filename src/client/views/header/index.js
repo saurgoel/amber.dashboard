aafad67d2bd45e9475322e5c2c2f13424f12481b
Tushar Kant <nanu.clickity@gmail.com>
2015-12-28 07:04:07 +0530

    header update

--- src/client/views/header/index.js
+++ src/client/views/header/index.js
@@ -1,5 +1,6 @@
 import {LayoutView} from 'marionette';
 import Radio from 'radio';
+import {UseCSS} from 'behaviors';
 
 import tpl from './template.jade';
 import style from './style.styl';
@@ -10,19 +11,24 @@ const HeaderChannel = Radio.channel('header');
 var HeaderView = LayoutView.extend({
 	template: tpl,
 	className: 'view-header',
+	behaviors: {UseCSS: {style}},
 	regions: {
 		SubHeader: '#region-sub-header'
 	},
 	ui: {
 		menuIcon: '.sidebar-toggle',
+		subpanel: '.subpanel'
 	},
 	events: {
 		'click @ui.menuIcon': 'toggleSidebar'
 	},
 
 	initialize(){
-		this.listenTo(this, 'render' , style.use);
-		this.listenTo(this, 'destroy', style.unuse);
+		this.listenTo(HeaderChannel, 'update:sub-title', this.updateSubtitle);
+	},
+
+	updateSubtitle(title = ''){
+		this.ui.subpanel.text(title)
 	},
 	toggleSidebar(e){
 		Radio.trigger('global', 'sidebar:toggle')
