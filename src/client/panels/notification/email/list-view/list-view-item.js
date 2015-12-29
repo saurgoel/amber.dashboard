9dbaddfb7763ccb9c942efc62242e1ec975ab320
Tushar Kant <nanu.clickity@gmail.com>
2015-12-30 00:37:39 +0530

    list view update

--- src/client/panels/notification/email/list-view/list-view-item.js
+++ src/client/panels/notification/email/list-view/list-view-item.js
@@ -1,13 +1,21 @@
 import {ItemView} from 'marionette';
+import {UseCSS} from 'behaviors';
+
 import tpl from './template-list-item.jade';
+import style from './style-item.styl';
+
 
 var EmailItem = ItemView.extend({
 	template: tpl,
   tagName: 'li',
-	className: 'view-email-item',
+  className: 'view-email-item',
+
+  behaviors: {UseCSS: {style}},
+
   ui: {
     item: '.item-inner'
   },
+
   events: {
     'click @ui.item': 'selectEmail'
   },
--- src/client/panels/notification/email/list-view/list-view.js
+++ src/client/panels/notification/email/list-view/list-view.js
@@ -18,12 +18,26 @@ var NotificationEmailsView = CompositeView.extend({
 
   emptyView: EmptyView,
 	childView: ChildView,
-	childViewContainer: '.list-wrapper ul.emails-list',
+	childViewContainer: 'ul.emails-list',
 	collection: new Collection(),
 
 	initialize(options){
 		this.collection.fetch();
 		this.listenTo(EmailChannel, 'item:selected', this.updateList);
+		// this.listenTo(this, 'render', this.fixScroll);
+	},
+
+	templateHelpers(){
+		return {
+			email_types: ['Transanctional', 'Promotional']
+		}
+	},
+
+	fixScroll(){
+		let $el = this.$('.list-wrapper ul.emails-list');
+		$el.each(function(){
+			$(this).css('height', $(window).height() - $el.get(0).getBoundingClientRect().top)
+		})
 	},
 
 	updateList(selected){
