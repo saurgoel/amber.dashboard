3d1c716e96c182f6f13e7504ada03c49d852668f
Tushar Kant <nanu.clickity@gmail.com>
2015-12-30 16:21:40 +0530

    products_api_url in config

--- config/index.js
+++ config/index.js
@@ -4,5 +4,6 @@ export default {
   api_base: 'https://product.getamber.com/',
   accounts_api_url: 'https://clients.getamber.com',
   accounts_api_internal_url: 'http://clients.internal.getamber.com',
-  notifications_api_url: 'https://notify.getamber.com'
+  notifications_api_url: 'https://notify.getamber.com',
+  products_api_url: 'https://product.getamber.com'
 };
