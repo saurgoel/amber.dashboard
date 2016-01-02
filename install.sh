c105e6f60e91a9a899b74a5455d5e8a58f427a48
Tushar Kant <nanu.clickity@gmail.com>
2016-01-02 17:09:03 +0530

    fix packages ['font-awesome', 'animate.css']
    these modules didn’t export proper files in “main”.

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
 # Install NPM Deps
-npm install
+# npm install
+
+echo "" \
+  "var fixes = {};" \
+  "fixes['font-awesome'] = 'css/font-awesome.min.css';"\
+  "fixes['animate.css'] = 'animate.min.css';"\
+  "Object.keys(fixes).forEach(function(dep){"\
+  "  var j = require(dep + '/package.json');"\
+  "  j.main = fixes[dep];"\
+  "  require('fs').writeFileSync('./node_modules/'+dep+'/package.json', JSON.stringify(j), 'utf-8');"\
+  "  console.log('Fixed: ' + dep );"\
+  "});" | node
