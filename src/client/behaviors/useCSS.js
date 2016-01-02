807868647dab46e0e237da31c84966b0bd615aa9
Tushar Kant <nanu.clickity@gmail.com>
2016-01-02 17:11:06 +0530

    useCSS now injects locals in children of CompositeView and CollectionView
    requiring them in each child only causes css bloat.
    also… :local(.self-child) applied automatically to children views

--- src/client/behaviors/useCSS.js
+++ src/client/behaviors/useCSS.js
@@ -1,33 +1,68 @@
 import _ from 'underscore';
-import {Behavior} from 'marionette';
+import {Behavior, CollectionView, CompositeView} from 'marionette';
 
 
 /** [UseCSS Inject styles and use local css vars through templateHelpers]
 */
+
 var UseCSS = Behavior.extend({
 	defaults: {
-		style: null
+		style: null,
+		inSelf : 'self',
+		inChild: 'self-child',
 	},
 
 	initialize(){
-		if (!this.options.style) return;
+		if (!this.options.style && _.isFunction(this.options.style))
+			return;
+
 		this.options.style.use();
+		this.parseLocals(this.options.style.use());
+		this.injectHelpers(this.view, this.locals);
+		this.injectClassName(this.view, this.inSelf);
 
-		var th = _.result(this.view, 'templateHelpers');
-		var locals = this.options.style.locals;
-		_.isUndefined(th)
-			? this.view.templateHelpers = {s:locals}
-			: this.view.templateHelpers = _.extend({}, th, {s:locals});
+		if( !(
+			(this.view instanceof CollectionView) ||
+			(this.view instanceof CompositeView)
+		)) return
 
-		// Export of .self
-		// directly to this.el element
-		// or extend this.className otherwise
-		let klass = _.has(locals, 'self') ? locals.self : '';
-		this.view.className = `${this.view.className || ''} ${klass}`;
+		this.listenTo(this.view, 'before:add:child', this.handleChildInjection)
 
-		if (this.view.isRendered)
-			this.view.el.className = `${this.view.el.className || ''} ${klass}`;
 	},
+
+	parseLocals({locals = {}}){
+		this.inSelf  = locals[this.options.inSelf]  || '';
+		this.inChild = locals[this.options.inChild] || '';
+
+		// Omit self classes and inject rest
+		this.locals  = _.omit(locals, this.options.inSelf, this.options.inChild);
+	},
+
+	handleChildInjection(childView){
+		this.injectHelpers(childView , this.locals);
+		this.injectClassName(childView, this.inChild);
+	},
+
+
+	injectHelpers(view, locals){
+		var th = _.result(view, 'templateHelpers');
+		_.isUndefined(th)
+			? view.templateHelpers = {s:locals}
+			: view.templateHelpers = _.extend({}, th, {s:locals});
+	},
+
+
+	injectClassName(view, klass){
+		view.className = this.extendStr(view.className, klass);
+		if (_.isElement(view.el))
+			view.el.className = this.extendStr(view.el.className, klass);
+	},
+
+
+	extendStr(str, value){
+		return _.isString(str) && _.isString(value) ? `${str} ${value}` : value;
+	},
+
 
 	onBeforeDestroy(){
 		if (!this.options.style) return;
