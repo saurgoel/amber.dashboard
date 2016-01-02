b62b3553e46ae58cde7d0aa40d08052171a8db7d
Tushar Kant <nanu.clickity@gmail.com>
2016-01-02 23:50:10 +0530

    provider list view

---
+++ src/client/panels/product/providers/list-view/index.js
@@ -0,0 +1,63 @@
+import {CompositeView} from 'marionette';
+import {UseCSS} from 'behaviors';
+import {_Notification} from 'channels';
+
+import tpl from './template.jade';
+import style from './style.styl';
+
+import ListItem from './list-item';
+import Collection from './collection';
+import Model from './model';
+
+var ProvidersListView = CompositeView.extend({
+
+  template: tpl,
+  behaviors: {UseCSS: {style}},
+
+  childView: ListItem,
+  childViewContainer: 'ul.providers-list',
+  collection: new Collection,
+  model: new Model,
+
+  ui: {
+    filter: '.tab.filter',
+    list: 'ul.providers-list'
+  },
+
+  events: {
+    'click @ui.filter': 'selectFilter'
+  },
+
+  initialize(options){
+    this.initial_id = options.id ? parseInt(options.id): null;
+
+    this.collection.fetch();
+    this.listenTo(this.collection, 'sync', this.onCollectionSync);
+    this.listenTo(_Notification, 'provider:selected', this.setItemActive);
+  },
+
+  onCollectionSync(){
+    if (!this.initial_id)
+      return
+    var model = this.collection.findWhere({id: this.initial_id})
+    _Notification.trigger('provider:selected', model);
+  },
+
+
+  setItemActive(model){
+    Backbone.history.navigate('product/providers/' + model.id)
+    this.children.each(function(view){
+      view.$el.toggleClass('is-active', model.id === view.model.id)
+    });
+  },
+
+  selectFilter(e){
+    let $el = $(e.currentTarget);
+    this.$(this.ui.filter.selector).each(function(){
+      $(this).toggleClass('is-active', $(this).is($el))
+    })
+  }
+
+})
+
+export default ProvidersListView;
