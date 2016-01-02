7d8f714405235ebf7b2ee238a0b0baac8402b522
Tushar Kant <nanu.clickity@gmail.com>
2016-01-02 21:39:31 +0530

    update providers

--- src/client/panels/product/providers/index.js
+++ src/client/panels/product/providers/index.js
@@ -1,6 +1,34 @@
-import {ItemView} from 'marionette';
+import {LayoutView} from 'marionette';
+import {UseCSS} from 'behaviors';
 
-var ProductProvidersView = ItemView.extend({
-  template: _.template('<h2> Product Providers </h2>')
+import tpl from './template.jade';
+import style from './style.styl';
+
+import ListView    from './list-view/index';
+import DetailsView from './details-view/index';
+
+
+
+var ProductProvidersView = LayoutView.extend({
+  template: tpl,
+  className: 'view-product-providers',
+  behaviors: {UseCSS:{style} },
+
+  regions: {
+    List: '.region-list',
+    Details: '.region-details'
+  },
+
+  initialize(options){
+    this.listview = new ListView(options);
+    this.detailsview = new DetailsView(options);
+    console.log('Prodivers: ', options)
+  },
+
+  onAttach(){
+    this.List.show(this.listview);
+    this.Details.show(this.detailsview);
+  }
+
 })
 export default ProductProvidersView;
