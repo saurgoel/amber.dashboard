596bbedf1196e89c7d32eba564c63a92258af5e0
Tushar Kant <nanu.clickity@gmail.com>
2016-01-04 21:26:45 +0530

    support params and counts

--- src/client/panels/product/providers/list-view/collection.js
+++ src/client/panels/product/providers/list-view/collection.js
@@ -1,29 +1,41 @@
 import {Model, Collection} from 'backbone';
 import {_Global} from 'channels';
+import moment from 'moment';
 
 const config = _Global.request('config');
+const apis = {
+  requests: `${config.products_api_url}/providers/requests.json`,
+  new     : `${config.products_api_url}/providers.json?status=new&verified=false`,
+  verified: `${config.products_api_url}/providers.json?status=new&verified=true&result=false`,
+  review  : `${config.products_api_url}/providers.json?status=review&result=false`,
+  active  : `${config.products_api_url}/providers.json?status=active&result=false`,
+}
 
 
 var ProductProvider = Model.extend({
-
   parse(resp){
-    return _.extend({}, resp.data, _.omit(resp, 'data'))
+    return _.extend({}, resp.data, _.omit(resp, 'data'), {
+      time_created: moment(resp.created_at).fromNow(),
+      time_updated: moment(resp.updated_at).fromNow()
+    })
   }
-
 });
 
 
 var ProductProviders  = Collection.extend({
   model: ProductProvider,
+  meta: {},
+  service: 'requests',
 
   url(){
-    return `${config.products_api_url}/providers/requests.json`
+    return apis[this.service];
   },
 
   parse(response){
     this.meta = response.data.meta;
     return response.data.results;
   }
+
 })
 
 export default ProductProviders;
--- src/client/panels/product/providers/list-view/model.js
+++ src/client/panels/product/providers/list-view/model.js
@@ -1,25 +1,43 @@
 import {Model} from 'backbone';
+import {_Global} from 'channels';
 
+const config = _Global.request('config');
 
-const filters = [
-  'All', 'New', 'Verified', 'Review', 'Active'
-].map(function(k){
-  return {
-    text:  k,
-    count: 0,
-    key: k.toLowerCase().replace(/\s/g, '_')
-  }
-});
+let filters = {
+  requests: {text: 'All', count: 0},
+  new: {text: 'New', count: 0},
+  verified: {text: 'Verified', count: 0},
+  review: {text: 'Review', count: 0},
+  active: {text: 'Active', count: 0},
+};
+
+let counts_api = {
+  new     : config.products_api_url + '/providers.json?status=new&verified=false&result=false',
+  verified: config.products_api_url + '/providers.json?status=new&verified=true&result=false',
+  review  : config.products_api_url + '/providers.json?status=review&result=false',
+  active  : config.products_api_url + '/providers.json?status=active&result=false',
+};
 
 var ProvidersViewModel = Model.extend({
   defaults: {
     selectedTab: null,
-    activeCount: 0,
-    verifiedCount: 0,
-    newCount: 0,
-    reviewCount: 0,
     filters: filters,
+    meta: {},
+  },
+
+  updateCounts(){
+    let keys = _.keys(counts_api);
+    let urls = _.values(counts_api);
+    return Promise
+      .all( urls.map( url => Promise.resolve($.getJSON(url)) ))
+      .then( responses => {
+        var _filters = {};
+        responses.map((k,i) => { _filters[keys[i]] = k.data.meta.count });
+        return _filters;
+      })
+      .catch(console.error.bind(console))
   }
+
 });
 
 export default ProvidersViewModel;
