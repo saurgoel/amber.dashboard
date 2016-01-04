b801ca82c2dd01955796726d4f8da0e6315d7080
Tushar Kant <nanu.clickity@gmail.com>
2016-01-04 21:27:29 +0530

    update list view

--- src/client/panels/product/providers/list-view/index.js
+++ src/client/panels/product/providers/list-view/index.js
@@ -1,6 +1,6 @@
 import {CompositeView} from 'marionette';
 import {UseCSS} from 'behaviors';
-import {_Notification} from 'channels';
+import {_Notification, _Global} from 'channels';
 
 import tpl from './template.jade';
 import style from './style.styl';
@@ -8,6 +8,8 @@ import style from './style.styl';
 import ListItem from './list-item';
 import Collection from './collection';
 import Model from './model';
+
+const config = _Global.request('config');
 
 var ProvidersListView = CompositeView.extend({
 
@@ -21,29 +23,83 @@ var ProvidersListView = CompositeView.extend({
 
   ui: {
     filter: '.tab.filter',
-    list: 'ul.providers-list'
+    list: 'ul.providers-list',
+
+    metarange: '.meta-range',
+    metacount: '.meta-count',
+
+    prev: '.go-prev',
+    next: '.go-next',
+    search: '.search-box input',
   },
 
   events: {
-    'click @ui.filter': 'selectFilter'
+    'click @ui.filter': 'selectFilter',
+    'click @ui.next': 'paginateNext',
+    'click @ui.prev': 'paginatePrev',
+    'keyup @ui.search': 'filterByText'
   },
 
   initialize(options){
+    window.aa = this;
     this.initial_id = options.id ? parseInt(options.id): null;
 
     this.listenTo(this.collection, 'sync', this.onCollectionSync);
+    this.listenTo(this.collection, 'sync', this.updateMeta);
     this.listenTo(_Notification, 'provider:selected', this.setItemActive);
 
     this.collection.fetch();
   },
 
+  filterByText(e){
+    let $el = $(e.currentTarget);
+    console.log('Filtering... ', $el.val())
+  },
+
+  onBeforeRender(){
+    this.model
+      .updateCounts()
+      .then(this.updateCounts.bind(this));
+  },
+
+  paginateNext(){
+    let {next, page} = this.collection.meta;
+    if (!next)
+      return
+    let p = page+1;
+    this.collection.fetch({ data: {p} });
+  },
+  paginatePrev(){
+    let {prev, page} = this.collection.meta;
+    if (!prev)
+      return
+    let p = page-1;
+    this.collection.fetch({ data: {p} });
+  },
+
   onCollectionSync(){
     if (!this.initial_id)
       return
-    var model = this.collection.findWhere({id: this.initial_id})
 
-    _Notification.trigger('provider:selected', model);
-    this.scrollToModel(model);
+    var model = this.collection.findWhere({id: this.initial_id})
+    if (model){
+      this.scrollToModel(model);
+      _Notification.trigger('provider:selected', model);
+    }
+    else{
+      console.log('No model in collection. id:', this.initial_id);
+    }
+  },
+
+  updateMeta(){
+    let {count, limit, prev, next, page} = this.collection.meta;
+
+    let start = prev ? prev*limit : 1;
+    let end   = next ? (next-1)*limit : count;
+
+    this.ui.metarange.text(`${start} - ${end}`);
+    this.ui.metacount.text(count)
+
   },
 
 
@@ -65,6 +121,16 @@ var ProvidersListView = CompositeView.extend({
     let $el = $(e.currentTarget);
     this.$(this.ui.filter.selector).each(function(){
       $(this).toggleClass('is-active', $(this).is($el))
+    });
+    this.collection.service = $el.attr('data-filter');
+    this.collection.fetch();
+  },
+
+  updateCounts(counts){
+    console.log('Updating counts...', counts);
+    this.ui.filter.each(function(){
+      var k = $(this).attr('data-filter');
+      $(this).find('.count').text( counts[k] );
     })
   }
 
